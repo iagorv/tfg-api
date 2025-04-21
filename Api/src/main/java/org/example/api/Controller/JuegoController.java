f98f6da68b220cc364f9cb49682520c0a2246bd3
iagorv <Iagorive18@gmail.com>
2025-04-21 12:52:03 +0200

    conexion db

--- Api/src/main/java/org/example/api/Controller/JuegoController.java
+++ Api/src/main/java/org/example/api/Controller/JuegoController.java
@@ -3,6 +3,9 @@ package org.example.api.Controller;
 
 import org.example.api.Entities.Juego;
 import org.example.api.Repository.JuegoRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
